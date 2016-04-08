8fb4d80fb2324a8d24774ba9f46b3fa840da020b
John Lim <jolim24601@gmail.com>
2016-04-08 11:42:55 -0400

    Have issue labels choose color of text based on contrast with background.

--- src/components/IssueLabels.js
+++ src/components/IssueLabels.js
@@ -2,6 +2,15 @@ import React, { Component, PropTypes } from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import { List } from 'immutable';
 
+// choose color of text based on contrast with background-color
+function getContrastYIQ(hexcolor){
+    var r = parseInt(hexcolor.substr(0,2),16);
+    var g = parseInt(hexcolor.substr(2,2),16);
+    var b = parseInt(hexcolor.substr(4,2),16);
+    var yiq = ((r*299)+(g*587)+(b*114))/1000;
+    return (yiq >= 128) ? 'black' : 'white';
+}
+
 export default class IssueLabels extends Component {
   static propTypes = {
     labels: PropTypes.instanceOf(List).isRequired
@@ -13,17 +22,14 @@ export default class IssueLabels extends Component {
   }
 
   renderLabel(label) {
-    let labelStyle = {
-      backgroundColor: `#${label.get('color')}`,
-      color: label.get('color') === 'FFFFFF' ? 'black' : 'white'
-    };
+    const bgColor = label.get('color');
 
     return (
       <span
         key={label.get('name')}
-        style={labelStyle}
+        style={{ backgroundColor: bgColor, color: getContrastYIQ(bgColor) }}
         className="label">
-          {label.get('name')}
+        {label.get('name')}
       </span>
     );
   }
